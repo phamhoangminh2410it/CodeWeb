224d5f28106876fa8262e3679f8f8d1f4405d619
Minh <125272514+phamhoangminh2410it@users.noreply.github.com>
2023-05-16 23:01:39 +0700

    Controller

---
+++ DSPhanCongController.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using QuanLySanXuatDuoc.Models;
+
+namespace QuanLySanXuatDuoc.Controllers
+{
+    public class DSPhanCongController : Controller
+    {
+        QuanLySanXuatXiNghiepDuocEntities db = new QuanLySanXuatXiNghiepDuocEntities();
+        // GET: DSPhanCong
+        public ActionResult Home()
+        {
+            return View();
+        }
+        public ActionResult Index()
+        {
+            var ds = from phancong in db.tPhanCongs select phancong;
+            return View(ds);
+        }
+
+        [HttpGet]
+        public ActionResult Edit(string id)
+        {
+            tPhanCong phancong = db.tPhanCongs.Find(id);
+            return View(phancong);
+        }
+        [HttpPost]
+        public ActionResult Edit(FormCollection f)
+        {
+            string ma = f.Get("SoPhanCong");
+            tPhanCong phancong = db.tPhanCongs.Find(ma);
+            phancong.MaPhanXuong = f.Get("MaPhanXuong");
+            phancong.SoPhieu = f.Get("SoPhieu");
+            phancong.MaThuoc = f.Get("MaThuoc");
+            phancong.SoLuong = f.Get("SoLuong");
+            db.SaveChanges();
+            return RedirectToAction("Index");
+
+        }
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(tPhanCong themphancong)
+        {
+            QuanLySanXuatXiNghiepDuocEntities db = new QuanLySanXuatXiNghiepDuocEntities();
+            db.tPhanCongs.Add(themphancong);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public ActionResult Find()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Find(FormCollection f)
+        {
+            string SoPhanCong = f.Get("SoPhanCong");
+            var phancong = db.tPhanCongs.Find("SoPhanCong");
+            return RedirectToAction("details/" + SoPhanCong);
+        }
+        public ActionResult Details(string id)
+        {
+            QuanLySanXuatXiNghiepDuocEntities db = new QuanLySanXuatXiNghiepDuocEntities();
+            tPhanCong mimi = db.tPhanCongs.Find(id);
+            return View(mimi);
+        }
+        [HttpGet]
+        public ActionResult Delete(string id)
+        {
+            tPhanCong phancong = db.tPhanCongs.Find(id);
+            return View(phancong);
+        }
+        [HttpPost]
+        public ActionResult Delete(FormCollection f)
+        {
+            string sophancong = f.Get("SoPhanCong");
+            var cthd = (from t in db.tPhanCongs where t.SoPhanCong == sophancong select t).FirstOrDefault();
+            {
+                tPhanCong phancong = db.tPhanCongs.Find(sophancong);
+                db.tPhanCongs.Remove(phancong);
+                db.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+
+    }
+}
---
+++ DSPhanXuongController.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using QuanLySanXuatDuoc.Models;
+
+namespace QuanlySanXuatDuoc.Controllers
+{
+    public class DSPhanXuongController : Controller
+    {
+        QuanLySanXuatXiNghiepDuocEntities db = new QuanLySanXuatXiNghiepDuocEntities();
+        // GET: DSPhanXuong
+        public ActionResult Home()
+        {
+            return View();
+        }
+        public ActionResult Index()
+        {
+            var ds = from phanxuong in db.tPhanXuongs select phanxuong;
+            return View(ds);
+        }
+
+        public ActionResult Edit(string id)
+        {
+            tPhanXuong phanxuong = db.tPhanXuongs.Find(id);
+            return View(phanxuong);
+        }
+        [HttpGet]
+        public ActionResult Delete(string id)
+        {
+            tPhanXuong phanxuong = db.tPhanXuongs.Find(id);
+            return View(phanxuong);
+        }
+        [HttpPost]
+        public ActionResult Delete(FormCollection f)
+        {
+            string id = f.Get("MaPhanXuong");
+            var px = (from ds in db.tPhanCongs where ds.MaPhanXuong == id select ds).FirstOrDefault();
+            if (px == null)
+            {
+                tPhanXuong phanxuong = db.tPhanXuongs.Find(id);
+                db.tPhanXuongs.Remove(phanxuong);
+                db.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+        [HttpPost]
+        public ActionResult Edit(FormCollection f)
+        {
+            string ma = f.Get("MaPhanXuong");
+            tPhanXuong phanxuong = db.tPhanXuongs.Find(ma);
+            phanxuong.TenPhanXuong = f.Get("TenPhanXuong");
+            phanxuong.DiaChi = f.Get("DiaChi");
+            phanxuong.DienThoai = f.Get("DienThoai");
+            phanxuong.HoTenQuanDoc = f.Get("HoTenQuanDoc");
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(tPhanXuong themphanxuong)
+        {
+            QuanLySanXuatXiNghiepDuocEntities db = new QuanLySanXuatXiNghiepDuocEntities();
+            db.tPhanXuongs.Add(themphanxuong);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult Details(string id)
+        {
+            QuanLySanXuatXiNghiepDuocEntities db = new QuanLySanXuatXiNghiepDuocEntities();
+            tPhanXuong chitiet = db.tPhanXuongs.Find(id);
+            return View(chitiet);
+        }
+        [HttpGet]
+        public ActionResult Find()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Find(FormCollection f)
+        {
+            string MaPhanXuong = f.Get("MaPhanXuong");
+            var phanxuong = db.tPhanXuongs.Find("MaPhanXuong");
+            return RedirectToAction("details/" + MaPhanXuong);
+        }
+    }
+}
